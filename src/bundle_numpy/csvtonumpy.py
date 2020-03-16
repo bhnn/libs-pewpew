c0343b68ea2a489aac8f08b55ae4ab7339c3f836
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2020-03-16 15:06:12 +0100

    Removed sub-array structure of np files, using npz archives now. Also fix to filename split

--- src/bundle_numpy/csvtonumpy.py
+++ src/bundle_numpy/csvtonumpy.py
@@ -5,25 +5,19 @@ import tqdm
 import shutil
 from tqdm import tqdm
 
-my_path = r'/Volumes/Samsung_T5/LIBSqORE_Austausch/'
+my_path = r'/media/ben/Volume/ml_data/hh_raw'
 
 
 def convert_to_numpy(datasetname):
-
-    files = sorted(glob.glob(os.path.join(my_path, datasetname+'/*.csv')))
-    print(os.path.join(my_path, datasetname+'/*.csv'))
-    for f in files:
+    files = sorted(glob.glob(os.path.join(my_path, datasetname ,'*.csv')))
+    for f in tqdm(files, desc=datasetname):
         if os.path.getsize(f) > 250000: # > 250 kB
-            filename = f[:-4] #filename without .csv
             data = np.loadtxt(f, skiprows=1, delimiter=',')
-            #data = np.delete(sample, [0], axis=1) #deletes wavelength
-            os.remove(f) #delete csv files
-            mineral_class = f[-21:-18]
-            mineral_subgroup = f[-17:-14]
-            mineral_id = f[-26:-22]
+            mineral_id, mineral_class, mineral_subgroup, _, filename = f.split('/')[-1].split('_')
+            filename = filename[:-4] # filename without .csv
             labels = np.asarray((mineral_class, mineral_subgroup, mineral_id)).astype(int)
-            output = np.asarray([data, labels])
-            np.save(filename+'.npy', output)
+            np.savez(filename, data=data, labels=labels)
+            os.remove(f) #delete csv files
         else:
             os.remove(f)
 
@@ -67,7 +61,7 @@ minerals_all = [(1, 'LIBS105'), (2, 'LIBS005 LIBS119'), (3, 'LIBS103'), (4, 'LIB
             (42, 'LIBS128'), (43, 'LIBS145'), (44, 'LIBS118'), (45, 'LIBS055'), (46, 'LIBS097'), (47, 'LIBS161'), (49, 'LIBS099'), (50, 'LIBS138'), (52, 'LIBS174'),
             (53, 'LIBS172'), (54, 'LIBS136'), (55, 'LIBS109'), (56, 'LIBS152'), (57, 'LIBS188'), (58, 'LIBS163'), (59, 'LIBS191'), (60, 'LIBS122'),
             (61, 'LIBS150'), (62, 'LIBS094'), (63, 'LIBS151'), (64, 'LIBS173'), (65, 'LIBS056 LIBS180'), (66, 'LIBS116'), (67, 'LIBS182'), (69, 'LIBS057 LIBS058 LIBS171'),
-            (70, 'LIBS189'), (71, 'LIBS130'), (72, 'LIBS133'), (73, 'LIBS059 LIBS060 LIBS061 LIBS062 LIBS063 LIBS064 LIBS065 LIBS066 LIBS067 LIBS068 LIBS069 LIBS070 LIBS071 LIBS072 LIBS073 LIBS074 LIBS075'), # malachite (74, 'LIBS110'),
+            (70, 'LIBS189'), (71, 'LIBS130'), (72, 'LIBS133'), (73, 'LIBS059 LIBS060 LIBS061 LIBS062 LIBS063 LIBS064 LIBS065 LIBS066 LIBS067 LIBS068 LIBS069 LIBS070 LIBS071 LIBS072 LIBS073 LIBS074 LIBS075'), (74, 'LIBS110'),
             (75, 'LIBS179'), (76, 'LIBS076 LIBS114'), (77, 'LIBS193'), (78, 'LIBS156'), (79, 'LIBS190'),
             (80, 'oliveni LIBS077 LIBS078 LIBS079 LIBS135'), (81, 'LIBS157'), (82, 'LIBS146'), (84, 'LIBS177'), (85, 'LIBS142'), (86, 'pseudomalachi LIBS081 LIBS082 LIBS175'),  (87, 'LIBS132'),
             (88, 'rosasi LIBS192'),  (89, 'LIBS096'), (90, 'LIBS084 LIBS134'), (91, 'LIBS131'), (92, 'LIBS085 LIBS086 LIBS100 LIBS100'), (94, 'LIBS149'), (95, 'LIBS087'),
@@ -85,13 +79,12 @@ def train_test_split(datasetname, dataset_id):
     test_labels = list()
     split  = 0.33
 
-    print(datasetname)
-    for m_id in tqdm(dataset_id):
+    for m_id in tqdm(dataset_id, desc=datasetname):
         m_id = '{0:04d}'.format(m_id)
         files = sorted(glob.glob(os.path.join(path, m_id+'*.npy')))
         max_mp = int(files[-1][-13:-10])+1 # max measure points, plus 1 because it starts counting from 0
 
-        for f in files:
+        for f in tqdm(files, leave=False):
             if int(f[-13:-10])+1 <= round(max_mp * split):
                 try:
                     os.makedirs(os.path.join(path, 'test_data'))
