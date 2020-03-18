47af818fc1736f9714c23917c2224d31ca3bf99f
Benjamin Henne <13474968+benjaminhenne@users.noreply.github.com>
2020-03-18 16:22:41 +0100

    Added progress bars

--- src/handheld_dataset/organise_hh_dataset.py
+++ src/handheld_dataset/organise_hh_dataset.py
@@ -3,8 +3,7 @@ from pathlib import Path
 from shutil import copy2
 
 import numpy as np
-
-
+from tqdm import tqdm
 
 minerals_12 = [(11, 'LIBS002 LIBS006 LIBS007 LIBS008 LIBS009 LIBS010 LIBS011 LIBS012 LIBS013 LIBS014 LIBS015 LIBS016 LIBS017 LIBS018 LIBS019 LIBS020'), # azurite
             (26, 'LIBS028 LIBS029 LIBS030 LIBS031 LIBS032 LIBS033 LIBS034 LIBS035 LIBS036 LIBS037 LIBS038 LIBS039'), # chalcopyrite
@@ -38,7 +37,7 @@ minerals_all = [(1, 'LIBS105'), (2, 'LIBS005 LIBS119'), (3, 'LIBS103'), (4, 'LIB
             (42, 'LIBS128'), (43, 'LIBS145'), (44, 'LIBS118'), (45, 'LIBS055'), (46, 'LIBS097'), (47, 'LIBS161'), (49, 'LIBS099'), (50, 'LIBS138'), (52, 'LIBS174'),
             (53, 'LIBS172'), (54, 'LIBS136'), (55, 'LIBS109'), (56, 'LIBS152'), (57, 'LIBS188'), (58, 'LIBS163'), (59, 'LIBS191'), (60, 'LIBS122'),
             (61, 'LIBS150'), (62, 'LIBS094'), (63, 'LIBS151'), (64, 'LIBS173'), (65, 'LIBS056 LIBS180'), (66, 'LIBS116'), (67, 'LIBS182'), (69, 'LIBS057 LIBS058 LIBS171'),
-            (70, 'LIBS189'), (71, 'LIBS130'), (72, 'LIBS133'), (73, 'LIBS059 LIBS060 LIBS061 LIBS062 LIBS063 LIBS064 LIBS065 LIBS066 LIBS067 LIBS068 LIBS069 LIBS070 LIBS071 LIBS072 LIBS073 LIBS074 LIBS075'), # malachite (74, 'LIBS110'),
+            (70, 'LIBS189'), (71, 'LIBS130'), (72, 'LIBS133'), (73, 'LIBS059 LIBS060 LIBS061 LIBS062 LIBS063 LIBS064 LIBS065 LIBS066 LIBS067 LIBS068 LIBS069 LIBS070 LIBS071 LIBS072 LIBS073 LIBS074 LIBS075'), (74, 'LIBS110'),
             (75, 'LIBS179'), (76, 'LIBS076 LIBS114'), (77, 'LIBS193'), (78, 'LIBS156'), (79, 'LIBS190'),
             (80, 'oliveni LIBS077 LIBS078 LIBS079 LIBS135'), (81, 'LIBS157'), (82, 'LIBS146'), (84, 'LIBS177'), (85, 'LIBS142'), (86, 'pseudomalachi LIBS081 LIBS082 LIBS175'),  (87, 'LIBS132'),
             (88, 'rosasi LIBS192'),  (89, 'LIBS096'), (90, 'LIBS084 LIBS134'), (91, 'LIBS131'), (92, 'LIBS085 LIBS086 LIBS100 LIBS100'), (94, 'LIBS149'), (95, 'LIBS087'),
@@ -51,15 +50,15 @@ def organise_datasets(minerals, outputname):
     Mineral-ID_Class_Subgroup_Measurepoint_Shot.csv
     """
     # file locations
-    minerals_file = r'/Users/jh/github/libs-pewpew/data/synthetic_minerals.npy'
-    hh_data = r'/Volumes/Samsung_T5/LIBSqORE_Austausch'
+    minerals_file = r'data/synthetic_minerals.npy'
+    hh_data = r'/media/ben/Volume/ml_data/hh_raw'
     output_path = os.path.join(hh_data, outputname)
 
     # information on all minerals
     all_list = np.load(minerals_file, allow_pickle=True)
     # globbed filenames of all .csv files in the data directory and subdirectories
 
-    for mineral_id, mineral_code in minerals:
+    for mineral_id, mineral_code in tqdm(minerals):
         codes = mineral_code.split(' ')
         filepaths = list()
         # iterate over entire directory structure to filter out spectra that belong to the current mineral
@@ -72,7 +71,6 @@ def organise_datasets(minerals, outputname):
                 for c in codes:
                     if c.lower() in f_str:
                         filepaths.append(f)
-        print('Found', len(filepaths),'spectra for mineral', all_list[mineral_id][1])
         unique_names = list()
         counter = -1
         sorted_by_parent = list()
@@ -96,7 +94,7 @@ def organise_datasets(minerals, outputname):
             os.makedirs(output_path)
 
         with open(os.path.join(hh_data, 'transformation_log_'+outputname+'.txt'), 'a') as logfile:
-            for directory in sorted_by_parent:
+            for directory in tqdm(sorted_by_parent, leave=False):
                 # write log file entry to mark the transformation of directory names to numeric indices in case anyone needs
                 # this information later
                 dir_name = str(directory[0].parent)[15:]
@@ -120,8 +118,6 @@ def organise_datasets(minerals, outputname):
                                                                                 shot_id)
                     copy2(str(f), os.path.join(output_path, new_file_name))
                 output_counter += 1
-        print('Finished with mineral:', all_list[mineral_id][1])
-
 
 organise_datasets(minerals=minerals_all, outputname = 'hh_all')
 organise_datasets(minerals=minerals_6, outputname = 'hh_6')
