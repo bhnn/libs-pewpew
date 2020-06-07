cd46217b5fa4bccb07d5dbcf2c585583f6705133
Julia Hansmeyer <julia_hansmeyer@hotmail.com>
2020-06-07 11:57:11 +0200

    Converting .npy files to .npz files

--- src/synthetic_dataset/npy_to_npz.py
+++ src/synthetic_dataset/npy_to_npz.py
@@ -6,10 +6,8 @@ import tqdm
 def convert_to_npz(path):
     files = sorted(glob.glob(os.path.join(path,'*.npy')))
     for f in files:
-        data = np.load(f, allow_pickle=True)
+        data, labels = np.load(f, allow_pickle=True)
         filename = f[:-4] # filename without .npy
-        mineral_id, mineral_class, mineral_subgroup, _, _ = f.split('/')[-1].split('_')
-        labels = np.asarray((mineral_class, mineral_subgroup, mineral_id)).astype(int)
         np.savez_compressed(filename, data=data, labels=labels)
         os.remove(f) #delete npy file
 
