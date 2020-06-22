3c5fab8ead7776a4dec99dcfad70d0e874449f25
Julia Hansmeyer <julia_hansmeyer@hotmail.com>
2020-06-22 20:35:19 +0200

    Final mixture results

--- src/visualisation/plot_mixture_results.py
+++ src/visualisation/plot_mixture_results.py
@@ -24,6 +24,7 @@ target_0 = np.load(os.path.join(path, 'data/synthetic_influence_target_0.npy'))
 target_1 = np.load(os.path.join(path, 'data/synthetic_influence_target_1.npy'))
 target_2 = np.load(os.path.join(path, 'data/synthetic_influence_target_2.npy'))
 
+
 mean0 = np.array([np.mean(y) for y in target_0])
 mean1 = np.array([np.mean(y) for y in target_1])
 mean2 = np.array([np.mean(y) for y in target_2])
@@ -41,29 +42,31 @@ plt.xlabel('Percentage of synthetic data added to handheld data', fontsize=10)
 plt.ylabel('Accuracy', fontsize= 14)
 
 axs[0].plot(x_values, mean0, label='Classes' , color= '#0d627a')
+axs[0].plot(x_values[np.argmax(mean0)], mean0.max(), 'o', color='black', markersize=4)
 axs[0].fill_between(x_values, mean0+std0, mean0-std0, alpha=0.5, facecolor= '#0d627a')
 axs[0].set_title('Classes', position=(0.9, 0.3), fontsize=10)
 axs[0].set_ylabel('Accuracy', fontsize=10)
 
 axs[1].plot(x_values, mean1, label='Groups'  , color= '#f2dc5c')
+axs[1].plot(x_values[np.argmax(mean1)], mean1.max(), 'o', color='black', markersize=4)
 axs[1].fill_between(x_values, mean1+std1, mean1-std1, alpha=0.5, facecolor='#f2dc5c')
 axs[1].set_title('Groups', position=(0.9, 0.3), fontsize=10)
 axs[1].set_ylabel('Accuracy', fontsize=10)
 
 axs[2].plot(x_values, mean2, label='Minerals', color= '#96cf6d')
+axs[2].plot(x_values[np.argmax(mean2)], mean2.max(), 'o', color='black', markersize=4)
 axs[2].fill_between(x_values, mean2+std2, mean2-std2, alpha=0.5, facecolor='#96cf6d')
 axs[2].set_title('Minerals', position=(0.9, 0.3), fontsize=10)
 axs[2].set_ylabel('Accuracy', fontsize=10)
 
 
 
-legend_elements = [Line2D([0], [0], color='black', lw=1, label='Average accuracy'),
-                   Patch(facecolor='black', alpha=0.5,
-                         label='Standard deviation')]
+legend_elements = [Line2D([0], [0], color='black', lw=2, label='Average accuracy'),
+                   Patch(facecolor='black', alpha=0.5,  label='Standard deviation'),
+                   Line2D([0], [0], marker='o', color='w', label='Maximum accuracy',
+                          markerfacecolor='black', markersize=6),]
 
-plt.legend(handles=legend_elements, loc='lower center', ncol=2, bbox_to_anchor=(0.5, -0.6))
+plt.legend(handles=legend_elements, loc='lower center', ncol=3, bbox_to_anchor=(0.5, -0.6))
 
-#plt.savefig(os.path.join(path, 'data/visualisations/mixture_results.pdf'))
-#plt.savefig(os.path.join(path, 'data/visualisations/mixture_results.png'))
-
-plt.show()
+plt.savefig(os.path.join(path, 'data/visualisations/mixture_results.pdf'))
+plt.savefig(os.path.join(path, 'data/visualisations/mixture_results.png'))
